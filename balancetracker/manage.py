3154c44083e27c7c58f736aae00531198a47e689
Nicolás Recalde <nrecalde@truora.com>
2023-06-06 00:35:16 -0500

    Check code format using github actions
    What: enforce code linting via github actions
    add some formats to gitignore
    format files using ruff
    Why: to pass the code pipeline
    
    BT-02

--- balancetracker/manage.py
+++ balancetracker/manage.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate, MigrateCommand
 
 from app import app, db
 
-#Commands to migrate
+# Commands to migrate
 migrate = Migrate(app, db)
 manager = Manager(app)
 
@@ -12,7 +12,7 @@ manager.add_command('db', MigrateCommand)
 if __name__ == '__main__':
     manager.run()
 
-#Follow these commands to migrate your database
-#python3 manage.py db init
-#python3  manage.py db migrate
-#python3 manage.py db upgrade
+# Follow these commands to migrate your database
+# python3 manage.py db init
+# python3  manage.py db migrate
+# python3 manage.py db upgrade
